a7887481f97910dfc048af39f9fae605d2a2f8f3
Vladimir Sadkov <sadkov@gmail.com>
2016-01-25 19:01:59 +0200

    makeindex.sh changed. not it compatible with cgid

--- makeindex.sh
+++ makeindex.sh
@@ -3,4 +3,6 @@
 name=`mysql -uadmin -padmin main -Ns -e 'select keyword from main where id="1"; '`
 buld=`cat /var/www/html/jenkins/buld.txt`
 
+echo "Content-type: text/html"
+echo ""
 echo "<html><title>Volodymyr Sadkov</title><body><h2>Hello,${name}!</h2><br><h5> buld number=${buld}. </h5></body></html>"
