abdc32bff7c34c150eecd4c57b827de287f6373c
Vladimir Sadkov <sadkov@gmail.com>
2016-01-25 17:28:53 +0200

    makeindex.sh changed. buld number should show correctly now

--- makeindex.sh
+++ makeindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 name=`mysql -uadmin -padmin main -Ns -e 'select keyword from main where id="1"; '`
-buld=`echo /var/www/html/jenkins/buld.txt`
+buld=`cat /var/www/html/jenkins/buld.txt`
 
 echo "<html><title>Volodymyr Sadkov</title><body><h2>Hello,${name}!</h2><br><h5> buld number=${buld}. </h5></body></html>"
